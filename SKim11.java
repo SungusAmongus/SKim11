8c3bc65c7ff3489bbcdf5682d70418d553bf7e81
Sung Kim <skim274@toromail.csudh.edu>
2023-11-10 02:30:53 +0000

    adding a java program

---
+++ SKim11.java
@@ -0,0 +1,17 @@
+// Sung Kim
+// CSC-321
+// Lab #11
+
+import java.util.Scanner;
+
+public class SKim11
+{
+	public static void main(String[] args)
+	{	
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int x = scanner.nextInt();
+		System.out.println("Your number squared is : " + x*x);
+	}
+}
+
